a9c5a38d47529756e38caad25878c57c70e1e576
Dylan Dotti <invocation42@yahoo.com>
2020-02-12 23:32:17 -0500

    Modified confidence levels for pokecenter_agent image searches

--- auto_trainer/pokecenter_agent.py
+++ auto_trainer/pokecenter_agent.py
@@ -14,7 +14,7 @@ class PokeCenterAgent:
     
     def handle_pokecenter(self):
         tc.activate_speedup()
-        while not va.is_in_window_half(self._target_url, 'top', confidence=0.95):
+        while not va.is_in_window_half(self._target_url, 'top', confidence=0.65):
             kc.press_up()
         kc.press_a()
         for _ in range(2):
@@ -24,13 +24,13 @@ class PokeCenterAgent:
         kc.press_a()
         self.wait_for_red_arrow()
         kc.press_a()
-        time.sleep(.66)
+        time.sleep(.5)
         kc.press_a()
         tc.deactivate_speedup()
         while (not va.is_in_window_half(
-            self._wht_arrow_url, 'bottom', 0.9) and
+            self._wht_arrow_url, 'bottom', 0.8) and
             not va.is_in_window_half(
-            self._grn_arrow_url, 'bottom', 0.9)):
+            self._grn_arrow_url, 'bottom', 0.8)):
             kc.press_down()
         kc.press_down()
         time.sleep(1)
@@ -42,11 +42,12 @@ class PokeCenterAgent:
     
     def wait_for_black_arrow(self):
         if not va.wait_for_one_image(self._blk_arrow_url, 
-            #region=wc.get_half_rect('right'), 
+            region=wc.get_half_rect('right'), 
             confidence=0.9):
             raise Exception('Failed to find black arrow')
 
 
 if __name__ == '__main__':
     wc.set_window_foreground_and_resize()
+    #PokeCenterAgent().wait_for_red_arrow()
     PokeCenterAgent().handle_pokecenter()
