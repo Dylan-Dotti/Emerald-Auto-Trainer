68d7422b42833f19c7522f2bc6c504b2692ae796
Dylan Dotti <invocation42@yahoo.com>
2020-04-02 18:53:38 -0400

    Update pokemon_current_moves_component.py

--- auto_trainer/gui/pokemon_current_moves_component.py
+++ auto_trainer/gui/pokemon_current_moves_component.py
@@ -13,18 +13,40 @@ class PokemonCurrentMovesComponent(tk.Frame):
         self._cbox_frame = tk.Frame(self)
         self._cbox_frame.grid(row=1, column=0)
 
-        moves = [m[0].title() for m in 
-            pmds.get_all_level_up_moves('charmander')]
-        self._move_0_cbox = ttk.Combobox(self._cbox_frame, 
+        #moves = [m[0].title() for m in 
+        #    pmds.get_all_level_up_moves('charmander')]
+        moves = [m['move']['name'].title() for m in
+            pmds.get_all_learnable_moves('charmander')]
+        self._cboxes = [ttk.Combobox(self._cbox_frame, 
             state='readonly', values=moves, style=combo_style)
-        self._move_1_cbox = ttk.Combobox(self._cbox_frame, 
-            state='readonly', values=moves, style=combo_style)
-        self._move_2_cbox = ttk.Combobox(self._cbox_frame, 
-            state='readonly', values=moves, style=combo_style)
-        self._move_3_cbox = ttk.Combobox(self._cbox_frame, 
-            state='readonly', values=moves, style=combo_style)
+            for _ in range(4)]
+
+        self._cboxes[0].bind('<<ComboboxSelected>>',
+            lambda e: self._on_move_changed(0))
+        self._cboxes[1].bind('<<ComboboxSelected>>',
+            lambda e: self._on_move_changed(1))
+        self._cboxes[2].bind('<<ComboboxSelected>>',
+            lambda e: self._on_move_changed(2))
+        self._cboxes[3].bind('<<ComboboxSelected>>',
+            lambda e: self._on_move_changed(3))
         
-        self._move_0_cbox.grid(row=0, column=0)
-        self._move_1_cbox.grid(row=0, column=1)
-        self._move_2_cbox.grid(row=1, column=0)
-        self._move_3_cbox.grid(row=1, column=1)
+        self._cboxes[0].grid(row=0, column=0)
+        self._cboxes[1].grid(row=0, column=1)
+        self._cboxes[2].grid(row=1, column=0)
+        self._cboxes[3].grid(row=1, column=1)
+    
+    def get_move(self, move_index):
+        return self._cboxes[move_index].get().lower()
+    
+    def set_move(self, move_index, move):
+        self._cboxes[move_index].set(move)
+    
+    def get_all_moves(self):
+        return [self.get_move(i) for i in range(4)]
+    
+    def _on_move_changed(self, move_index):
+        new_move = self.get_move(move_index)
+        if new_move != '':
+            for i in [x for x in range(4) if x != move_index]:
+                if self.get_move(i) == new_move:
+                    self.set_move(i, '')
