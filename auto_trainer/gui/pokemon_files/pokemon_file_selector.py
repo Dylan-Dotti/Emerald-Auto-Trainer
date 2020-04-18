5917e2f314a054b899b82cfe3603166ffe136457
Dylan-Dotti <invocation42@yahoo.com>
2020-04-18 14:08:13 -0400

    Added PokemonFileSelector and PokemonFileDataService

---
+++ auto_trainer/gui/pokemon_files/pokemon_file_selector.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class PokemonFileSelector(tk.Frame):
+
+    def __init__(self, master):
+        super().__init__(master)
+        
---
+++ auto_trainer/services/pokemon_files_data_service.py
@@ -0,0 +1,5 @@
+import auto_trainer.services.pokemon_data_directory_service as pdds
+
+
+def get_all_pokemon_file_names():
+    pass
