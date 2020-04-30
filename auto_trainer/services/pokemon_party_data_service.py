544ea16de4eaf1eea927fb52bb0af414a67a8d00
Dylan Dotti <invocation42@yahoo.com>
2020-04-30 16:27:38 -0400

    Modified load_party method

--- auto_trainer/services/pokemon_party_data_service.py
+++ auto_trainer/services/pokemon_party_data_service.py
@@ -1,12 +1,18 @@
 # loads/saves pokemon data from/to the party_pokemon data file
 import auto_trainer.services.directory_service as ds
 import auto_trainer.services.json_data_service as jds
+import auto_trainer.services.pokemon_file_service as pfs
 import os
-import pokemon
+import auto_trainer.pokemon
 
 
 def load_party():
-    return jds.load_data(_party_file_url)
+    name_id_pairs = jds.load_data(_party_file_url)
+    party_pokemon = [pfs.load_pokemon_from_file(n, i) 
+        for (n, i) in name_id_pairs]
+    for p in party_pokemon:
+        print(p.get_name())
+    return party_pokemon
 
 
 def save_party(party_dict):
