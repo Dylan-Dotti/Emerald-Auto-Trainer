2bf10b36ffd74273fef2a73cfa1e00905937958f
Dylan Dotti <invocation42@yahoo.com>
2020-05-01 14:58:45 -0400

    Update emerald_listbox.py

--- auto_trainer/gui/emerald_listbox.py
+++ auto_trainer/gui/emerald_listbox.py
@@ -7,11 +7,22 @@ class EmeraldListbox(tk.Listbox):
         super().__init__(master, selectmode=selectmode,
             highlightcolor='green', selectbackground='green', 
             font=(None, 12), width=15, activestyle=None)
+    
+    def get_values(self):
+        return self.get(0, tk.END)
 
     def get_selected(self):
         return self.get(self.curselection())
     
     def pop_selected(self):
-        selected_val = self.get_selected()
-        self.delete(self.curselection())
-        return selected_val
+        return self.pop_index(self.curselection())
+    
+    def pop_item(self, item):
+        values = self.get_values()
+        index = values.index(item)
+        return self.pop_index(index)
+    
+    def pop_index(self, index):
+        value = self.get(index)
+        self.delete(index)
+        return value
